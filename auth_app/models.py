19b26e3723c85ab8e3977a2d34755dda09338938
Sergey Lobanov <0@tbot.pro>
2022-01-23 18:38:51 +0300

    model update

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,14 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class PortalUser(AbstractUser):
-    is_staff = None
-    is_superuser = None
-    groups = None
+    is_staff = models.BooleanField(_('staff'), null=True)
+    is_superuser = models.BooleanField(_('superuser'), null=True)
     phone = models.CharField(_('phone'), max_length=50, unique=True)
     email = models.EmailField(_('email address'), unique=True)
 
     REQUIRED_FIELDS = ['email', 'phone']
-
