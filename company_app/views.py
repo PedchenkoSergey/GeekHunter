e87318c9fb133e8a26e2fd9ebf7be09c6d6522e1
Sergey Lobanov <0@tbot.pro>
2022-01-24 16:25:08 +0300

    company app views & templates

--- company_app/views.py
+++ company_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.core import serializers
+from django.shortcuts import render, get_object_or_404
+
+from .models import Card
+
 
 # Create your views here.
+
+def get_company_card(request, pk=None):
+    card = get_object_or_404(Card, pk=pk)
+    card_data = serializers.serialize("python", Card.objects.filter(id=pk))
+    context = {
+        'title': 'company card',
+        'card': card,
+        'card_data': card_data,
+    }
+    if card.status == 'ACTIVE' and card.moderation_status == 'APPROVED':
+        return render(request, 'company_app/company_card.html', context)
+    else:
+        return render(request, 'company_app/company_card_not_available.html', context)
