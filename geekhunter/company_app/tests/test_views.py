84606c732fb94329532880f9714437b698810a72
d-karpov <77918194+d-karpov@users.noreply.github.com>
2022-02-03 02:16:07 +0300

    Add tests for vacancies
    - Add test for basic vacancies functional
    - Refactor Company App tests code (add response codes as constants)

--- geekhunter/company_app/tests/test_views.py
+++ geekhunter/company_app/tests/test_views.py
@@ -1,10 +1,17 @@
 from django.test import TestCase
 from django.test.client import Client
+from django.contrib.auth.models import Permission
 
-from company_app.models import Company, Card
+from auth_app.models import PortalUser
+from company_app.models import Company, Card, Vacancy
 
 
 class CompanyAppTestCase(TestCase):
+    SUCCESS_RESPONSE_CODE = 200
+    REDIRECT_RESPONSE_CODE = 302
+    PERMISSON_DENIED_CODE = 403
+    PAGE_NOT_FOUND_CODE = 404
+
     def setUp(self) -> None:
         self.client = Client()
         self.company_1 = Company.objects.create(name='company_1', short_description='descr', specialization='IT')
@@ -41,18 +48,43 @@ class CompanyAppTestCase(TestCase):
             moderation_status='APPROVED',
         )
 
+        self.user_credentials = {
+            'username': 'employee',
+            'email': 'employee@geekhunter.com',
+            'phone': '2345',
+            'password': 'password',
+        }
+
+        self.user = PortalUser.objects.create_user(**self.user_credentials)
+
     def test_company_card_page_approved_and_active_ok(self):
         response = self.client.get('/company/1/')
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(self.SUCCESS_RESPONSE_CODE, response.status_code)
 
     def test_company_card_page_not_approved_and_active_ok(self):
         response = self.client.get('/company/2/')
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(self.SUCCESS_RESPONSE_CODE, response.status_code)
 
     def test_company_card_page_approved_and_not_active_ok(self):
         response = self.client.get('/company/3/')
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(self.SUCCESS_RESPONSE_CODE, response.status_code)
 
     def test_company_card_404(self):
         response = self.client.get(f'/company/{Company.objects.all().count() + 1}/')
-        self.assertEqual(404, response.status_code)
+        self.assertEqual(self.PAGE_NOT_FOUND_CODE, response.status_code)
+
+    def test_vacancies_redirect_anonymous(self):
+        response = self.client.get('/company/vacancies')
+        self.assertEqual(self.REDIRECT_RESPONSE_CODE, response.status_code)
+
+    def test_vacancies_for_user_has_perm(self):
+        self.user.user_permissions.add(Permission.objects.get(codename='view_vacancy'))
+        self.client.login(username='employee', password="password")
+        response = self.client.get('/company/vacancies')
+        self.assertEqual(self.SUCCESS_RESPONSE_CODE, response.status_code)
+
+    def test_vacancies_for_user_has_no_perm(self):
+        self.client.login(username='employee', password="password")
+        response = self.client.get('/company/vacancies')
+        self.assertEqual(self.PERMISSON_DENIED_CODE, response.status_code)
+
