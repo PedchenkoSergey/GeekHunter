28c59d297a311f8f6f4051b9388eba2c8ae83eb9
Ruslan Zakirov <zakirovra@gmail.com>
2022-02-19 17:10:44 +0400

    - paginations news page

--- geekhunter/news_app/views.py
+++ geekhunter/news_app/views.py
@@ -10,12 +10,18 @@ class PostListView(ListView):
     model = News
     # Под данным именем наш список статей будет доступен в шаблоне
     context_object_name = 'news'
+    extra_context = {
+        'title': 'Новости',
+    }
+
+    paginate_by = 3
 
     # Название шаблона
     template_name = 'news_app/news_list.html'
 
     def get_queryset(self):
         return News.objects.filter(status='APPROVED').order_by('-created_at')
+
 
 class PostDetailView(DetailView):
     """
@@ -24,8 +30,14 @@ class PostDetailView(DetailView):
 
     model = News
 
+    queryset = News.objects.all()
     # Под данным именем наш список статей будет доступен в шаблоне
     context_object_name = 'post'
 
     # Название шаблона
     template_name = 'news_app/news_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context['title'] = self.get_object().title
+        return context
