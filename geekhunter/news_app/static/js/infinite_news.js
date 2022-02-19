cda18351e83a348a6ed0f1619c2686239647cc04
Ruslan Zakirov <zakirovra@gmail.com>
2022-02-19 15:36:52 +0400

    - delete static

--- geekhunter/news_app/static/js/infinite_news.js
+++
@@ -1,8 +0,0 @@
-let page = 1;
-let load = () => $.get('/?page=' + ++page, (data) => {
-    const html = $.parseHTML(data);
-    if (!$('.more', html).length) {
-        $('.more').hide()
-    }
-    $('.infinite-container').append($('.infinite-container', html).html());
-});
