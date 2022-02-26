3cf63c9c410dc2851f286a222d1e05e8cef32d04
Sergey Lobanov <0@tbot.pro>
2022-02-26 11:18:41 +0300

    update company card template, update company offer form

--- geekhunter/company_app/forms/CompanyOfferForm.py
+++ geekhunter/company_app/forms/CompanyOfferForm.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from employee_app.models import Resume
-from ..models import Offer
+from ..models import Offer, Vacancy
 
 
 class CompanyOfferForm(forms.ModelForm):
@@ -9,6 +9,7 @@ class CompanyOfferForm(forms.ModelForm):
         self.resume_id = kwargs.pop('resume_id')
         super(CompanyOfferForm, self).__init__()
         self.fields['resume'].queryset = Resume.objects.filter(id=self.resume_id)
+        self.fields['vacancy'].queryset = Vacancy.objects.filter(status='ACTIVE', moderation_status='APPROVED')
 
     class Meta:
         model = Offer
