6c66f7bdd429defe8b152148e6d8aeb4a0445e99
KrisMelikova <kristina@melikova.ru>
2022-02-26 19:57:42 +0300

    added resumes and vacancies to favorites

--- geekhunter/employee_app/views.py
+++ geekhunter/employee_app/views.py
@@ -183,7 +183,6 @@ class ResumesView(PermissionRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['favorite_resumes'] = FavoriteResume.objects.filter(hr_manager=self.request.user.id)
-
         return context
 
     def post(self, request, *args, **kwargs):
